3cf3e4cd30cb89787a3f426e960a94914489f16a
LouisKamp <louiskampeskildsen99@gmail.com>
2021-01-11 18:10:53 +0100

    remove test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable jest/expect-expect */
-import { render } from '@testing-library/react'
-import React from 'react'
-
-import { App } from './App'
-
-test('renders app', () => {
-    render(<App />)
-})
